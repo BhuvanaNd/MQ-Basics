d44bce278d83350ac736f89d9fa38411e6a60ecb
Bhuvana N <bhuvana@Bhuvanas-MacBook-Air.local>
2019-08-15 21:59:55 +0530

    Commit1 - With Sender and Receiver

---
+++ Receiver/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() {HostName="localhost"};
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {   
+                    channel.QueueDeclare(queue: "FirstQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, msg)=>
+                    {
+                        var body = msg.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Messge Received: {0} \n",message);
+                    };
+                    channel.BasicConsume(queue: "FirstQueue",
+                                         autoAck: true,
+                                         consumer: consumer);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() {HostName="localhost"};
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {   
+                    channel.QueueDeclare(queue: "FirstQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                    string message = "My First Message in Queue";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                     routingKey: "RabbitMQ",
+                                     basicProperties: null,
+                                     body: body);
+                    Console.WriteLine("Message is Sent");
+
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
